8744ca4e077786402802c77ae90304c395aed208
jonathon mullins <jonathonm93@gmail.com>
2020-10-28 08:05:25 -0600

    first commit

---
+++ js/dist/main.dev.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var question = {
+  stem: "who is buried in grant's tomb?",
+  option1: "boglue",
+  option2: "grant",
+  option3: "grant and boglue",
+  option4: "me",
+  correct: 2,
+  display: function display() {
+    document.querySelector('#stem').textContent = question.stem;
+    question.option1.textContent, question.option2.textContent, question.option3.textContent, question.option4.textContent;
+  },
+  check: function check(userChoice) {
+    if (userChoice === question.correct) {
+      document.querySelector(".feedback").textContent = "you are correct!";
+    } else document.querySelector(".feedback").textContent = "try again";
+  }
+};
+document.querySelector('#answer1').addEventListener('click', function () {
+  return question.check(1);
+});
+document.querySelector('#answer2').addEventListener('click', function () {
+  return question.check(2);
+});
+document.querySelector('#answer3').addEventListener('click', function () {
+  return question.check(3);
+});
+document.querySelector('#answer4').addEventListener('click', function () {
+  return question.check(4);
+});
+question.display();
+//# sourceMappingURL=main.dev.js.map
